848925f6491589709afe7c2656f21f4f2d95bdb3
Fulya34 <funda.sisek_49@icloud.com>
2026-01-12 13:15:47 +0300

    Ödev tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.enums.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("North");
+        Wall wall4 = new Wall("South");
+
+        Ceiling ceiling = new Ceiling(12, PaintColor.WHITE);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp(LampType.NEON, true, 85);
+
+        Wardrobe wardrobe = new Wardrobe(200, 180, 75.5);
+
+        Carpet carpet = new Carpet(300, 200, PaintColor.RED);
+
+        Bedroom bedroom = new Bedroom(
+                "Master Bedroom",
+                wall1, wall2, wall3, wall4,
+                ceiling,
+                bed,
+                lamp,
+                wardrobe,
+                carpet
+        );
+
+        // 🔽 METOD TESTLERİ
+        lamp.turnOn();
+        bed.make();
+        wardrobe.add();
+        carpet.lying();
+        ceiling.create();
+        wall1.create();
+
+        // 🔽 GETTER TESTLERİ
+        System.out.println("Bedroom name: " + bedroom.getName());
+        System.out.println("Lamp style: " + bedroom.getLamp().getStyle());
+        System.out.println("Carpet color: " + bedroom.getCarpet().getColor());
     }
 }
